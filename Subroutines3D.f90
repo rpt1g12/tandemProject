809e6dfff03c3f00a8ce11eaa34f5a6f4117cf27
R. Perez Torro <rpt1g12@soton.ac.uk>
2014-09-20 21:42:08 +0100

    Subroutines3D.f90 merged with kimAugust14 version.

--- Subroutines3D.f90
+++ Subroutines3D.f90
@@ -13,24 +13,25 @@
 
 !===== SUBROUTINE FOR MPI IMPLEMENTATION
 
- subroutine mpigo(nt,itag,ng)
+ subroutine mpigo(nt,nrt,n45,itag)
 
- integer,intent(in) :: nt,itag,ng
+ integer,intent(in) :: nt,nrt,n45,itag
 
  select case(nt); case(0); mp=lmd; case(1); mp=lmf; end select
 
     ir=0
- do nn=1,3
+ do nn=1,3; nz=(1-nrt)*(nn-1)+1
  select case(nn)
  case(1); send=>send1; recv=>recv1; case(2); send=>send2; recv=>recv2; case(3); send=>send3; recv=>recv3
  end select
- do ip=0,1; iq=1-ip; is=ip*ijk(1,nn); ie=1-2*ip; np=(nbc(ip,nn)-40)/5; ii=abs(np); npg=max(np,0)*ng
- if(ndf(ip,nn)==1) then
+ do ip=0,1; iq=1-ip; is=ip*ijk(1,nn); ie=1-2*ip
+ select case(nbc(ip,nn)); case(35); np=0; ii=1; case(40); np=0; ii=0; case(45); np=n45; ii=1; end select
+ if(ndf(ip,nt,nn)==1) then
  do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
  do j=0,ijk(2,nn); jk=kp+j; l=indx3(is,j,k,nn)
-    res=npg*rr(l,nn)
+    res=np*rr(l,nz)
  do i=0,mp; l=indx3(is+ie*(i+ii),j,k,nn)
-    sap(i)=rr(l,nn)
+    sap(i)=rr(l,nz)
  end do
     send(jk,0,ip)=sum(pbco(0:mp,0,nt)*sap(0:mp))-res*pbcot(0,nt)
     send(jk,1,ip)=sum(pbco(0:mp,1,nt)*sap(0:mp))-res*pbcot(1,nt)
@@ -46,16 +47,16 @@
     call MPI_WAITALL(ir,ireq,ista,ierr)
  end if
 
- if(ng==1) then
- do nn=1,3
+ if(n45==n45go) then
+ do nn=1,3; nz=(1-nrt)*(nn-1)+1
  select case(nn); case(1); recv=>recv1; case(2); recv=>recv2; case(3); recv=>recv3; end select
- do ip=0,1; is=ip*ijk(1,nn); np=(nbc(ip,nn)-40)/5
- if(np==1) then
+ do ip=0,1; is=ip*ijk(1,nn)
+ if(nbc(ip,nn)==45) then
  do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
  do j=0,ijk(2,nn); jk=kp+j; l=indx3(is,j,k,nn)
-    recv(jk,0,ip)=recv(jk,0,ip)+rr(l,nn)*pbcot(0,nt)
-    recv(jk,1,ip)=recv(jk,1,ip)+rr(l,nn)*pbcot(1,nt)
-    recv(jk,2,ip)=recv(jk,2,ip)+rr(l,nn)
+    recv(jk,0,ip)=recv(jk,0,ip)+rr(l,nz)*pbcot(0,nt)
+    recv(jk,1,ip)=recv(jk,1,ip)+rr(l,nz)*pbcot(1,nt)
+    recv(jk,2,ip)=recv(jk,2,ip)+rr(l,nz)
  end do
  end do
  end if
@@ -67,11 +68,11 @@
 
 !===== SUBROUTINE FOR COMPACT FINITE DIFFERENTIATING
 
- subroutine deriv(nn)
+ subroutine deriv(nn,nz)
 
- integer,intent(in) :: nn
+ integer,intent(in) :: nn,nz
 
-    nt=0; ns=ndf(0,nn); ne=ndf(1,nn)
+    nt=0; ns=ndf(0,0,nn); ne=ndf(1,0,nn)
 
  select case(nn)
  case(1); is=0; ie=is+lxi; recv=>recv1
@@ -82,10 +83,10 @@
  do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
  do j=0,ijk(2,nn); jk=kp+j
  do i=is,ie; l=indx3(i-is,j,k,nn)
-    li(i)=l; sa(i)=rr(l,nn)
+    li(i)=l; sa(i)=rr(l,nz)
  end do
  select case(ns)
- case(0)
+ case(-1,0)
     sb(is)=dot_product(abc(:,0),(/sa(is+1),sa(is+2),sa(is+3),sa(is+4),sa(is+5),sa(is+6)/)-sa(is))
     sb(is+1)=dot_product(abc(:,1),(/sa(is),sa(is+2),sa(is+3),sa(is+4),sa(is+5),sa(is+6)/)-sa(is+1))
     sb(is+2)=dot_product(abc(:,2),(/sa(is),sa(is+1),sa(is+3),sa(is+4),sa(is+5),sa(is+6)/)-sa(is+2))
@@ -98,7 +99,7 @@
     sb(i)=aa*(sa(i+1)-sa(i-1))+ab*(sa(i+2)-sa(i-2))+ac*(sa(i+3)-sa(i-3))
  end do
  select case(ne)
- case(0)
+ case(-1,0)
     sb(ie)=dot_product(abc(:,0),sa(ie)-(/sa(ie-1),sa(ie-2),sa(ie-3),sa(ie-4),sa(ie-5),sa(ie-6)/))
     sb(ie-1)=dot_product(abc(:,1),sa(ie-1)-(/sa(ie),sa(ie-2),sa(ie-3),sa(ie-4),sa(ie-5),sa(ie-6)/))
     sb(ie-2)=dot_product(abc(:,2),sa(ie-2)-(/sa(ie),sa(ie-1),sa(ie-3),sa(ie-4),sa(ie-5),sa(ie-6)/))
@@ -127,11 +128,11 @@
 
 !===== SUBROUTINE FOR COMPACT FILTERING
 
- subroutine filte(nn,m)
+ subroutine filte(nn)
 
- integer,intent(in) :: nn,m
+ integer,intent(in) :: nn
 
-    nt=1; ns=ndf(0,nn); ne=ndf(1,nn)
+    nt=1; ns=ndf(0,1,nn); ne=ndf(1,1,nn)
 
  select case(nn)
  case(1); is=0; ie=is+lxi; recv=>recv1
@@ -139,45 +140,43 @@
  case(3); is=lxi+let+2; ie=is+lze; recv=>recv3
  end select
 
+    fex(:)=(/45,-9,1/)/(mfbi*30.0_nr)
+
  do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
  do j=0,ijk(2,nn); jk=kp+j
  do i=is,ie; l=indx3(i-is,j,k,nn)
-    li(i)=l; sa(i)=qa(l,m)
+    li(i)=l; sa(i)=rr(l,1)
  end do
  select case(ns)
+ case(-1)
+    rof(:)=2*sa(is:is+2); res=sum(fex(:)*(sa(is+mfbi*(/1,2,3/))-sa(is))); rv(:)=sa(is)-res*(/1,2,3/)
+    sb(is)=fam(0)*(rv(1)+sa(is+1)-rof(0))+fbm(0)*(rv(2)+sa(is+2)-rof(0))+fcm(0)*(rv(3)+sa(is+3)-rof(0))
+    sb(is+1)=fam(1)*(sa(is)+sa(is+2)-rof(1))+fbm(1)*(rv(1)+sa(is+3)-rof(1))+fcm(1)*(rv(2)+sa(is+4)-rof(1))
+    sb(is+2)=fam(2)*(sa(is+1)+sa(is+3)-rof(2))+fbm(2)*(sa(is)+sa(is+4)-rof(2))+fcm(2)*(rv(1)+sa(is+5)-rof(2))
  case(0)
- if(nfbco==0) then
-    sb(is)=0; sb(is+1)=0
-    sb(is+2)=dot_product(fbc(:),(/sa(is),sa(is+1),sa(is+3),sa(is+4),sa(is+5)/)-sa(is+2))
- else
-    res=sum((/fex1,fex2,fex3/)*(sa(is+(/1,2,3/)*nfbcn)-sa(is))); rv(:)=sa(is)-(/1,2,3/)*res
-    res=2*sa(is); sb(is)=fa*(rv(1)+sa(is+1)-res)+fb*(rv(2)+sa(is+2)-res)+fc*(rv(3)+sa(is+3)-res)
-    res=2*sa(is+1); sb(is+1)=fa*(sa(is)+sa(is+2)-res)+fb*(rv(1)+sa(is+3)-res)+fc*(rv(2)+sa(is+4)-res)
-    res=2*sa(is+2); sb(is+2)=fa*(sa(is+1)+sa(is+3)-res)+fb*(sa(is)+sa(is+4)-res)+fc*(rv(1)+sa(is+5)-res)
- end if
+    sb(is:is+1)=0; sb(is+2)=dot_product(fbc(:),(/sa(is),sa(is+1),sa(is+3),sa(is+4),sa(is+5)/)-sa(is+2))
  case(1)
+    rof(2)=2*sa(is+2)
     sb(is)=sum(pbci(0:lmf,0,nt)*sa(is:is+lmf))+recv(jk,0,0)
     sb(is+1)=sum(pbci(0:lmf,1,nt)*sa(is:is+lmf))+recv(jk,1,0)
-    res=2*sa(is+2); sb(is+2)=fa*(sa(is+1)+sa(is+3)-res)+fb*(sa(is)+sa(is+4)-res)+fc*(recv(jk,2,0)+sa(is+5)-res)
+    sb(is+2)=fa*(sa(is+1)+sa(is+3)-rof(2))+fb*(sa(is)+sa(is+4)-rof(2))+fc*(recv(jk,2,0)+sa(is+5)-rof(2))
  end select
  do i=is+3,ie-3
     res=2*sa(i); sb(i)=fa*(sa(i-1)+sa(i+1)-res)+fb*(sa(i-2)+sa(i+2)-res)+fc*(sa(i-3)+sa(i+3)-res)
  end do
  select case(ne)
+ case(-1)
+    rof(:)=2*sa(ie:ie-2:-1); res=sum(fex(:)*(sa(ie)-sa(ie-mfbi*(/1,2,3/)))); rv(:)=sa(ie)+res*(/1,2,3/)
+    sb(ie)=fam(0)*(sa(ie-1)+rv(1)-rof(0))+fbm(0)*(sa(ie-2)+rv(2)-rof(0))+fcm(0)*(sa(ie-3)+rv(3)-rof(0))
+    sb(ie-1)=fam(1)*(sa(ie-2)+sa(ie)-rof(1))+fbm(1)*(sa(ie-3)+rv(1)-rof(1))+fcm(1)*(sa(ie-4)+rv(2)-rof(1))
+    sb(ie-2)=fam(2)*(sa(ie-3)+sa(ie-1)-rof(2))+fbm(2)*(sa(ie-4)+sa(ie)-rof(2))+fcm(2)*(sa(ie-5)+rv(1)-rof(2))
  case(0)
- if(nfbco==0) then
-    sb(ie)=0; sb(ie-1)=0
-    sb(ie-2)=dot_product(fbc(:),(/sa(ie),sa(ie-1),sa(ie-3),sa(ie-4),sa(ie-5)/)-sa(ie-2))
- else
-    res=sum((/fex1,fex2,fex3/)*(sa(ie)-sa(ie-(/1,2,3/)*nfbcn))); rv(:)=sa(ie)+(/1,2,3/)*res
-    res=2*sa(ie); sb(ie)=fa*(sa(ie-1)+rv(1)-res)+fb*(sa(ie-2)+rv(2)-res)+fc*(sa(ie-3)+rv(3)-res)
-    res=2*sa(ie-1); sb(ie-1)=fa*(sa(ie-2)+sa(ie)-res)+fb*(sa(ie-3)+rv(1)-res)+fc*(sa(ie-4)+rv(2)-res)
-    res=2*sa(ie-2); sb(ie-2)=fa*(sa(ie-3)+sa(ie-1)-res)+fb*(sa(ie-4)+sa(ie)-res)+fc*(sa(ie-5)+rv(1)-res)
- end if
+    sb(ie:ie-1:-1)=0; sb(ie-2)=dot_product(fbc(:),(/sa(ie),sa(ie-1),sa(ie-3),sa(ie-4),sa(ie-5)/)-sa(ie-2))
  case(1)
+    rof(2)=2*sa(ie-2)
     sb(ie)=sum(pbci(0:lmf,0,nt)*sa(ie:ie-lmf:-1))+recv(jk,0,1)
     sb(ie-1)=sum(pbci(0:lmf,1,nt)*sa(ie:ie-lmf:-1))+recv(jk,1,1)
-    res=2*sa(ie-2); sb(ie-2)=fa*(sa(ie-3)+sa(ie-1)-res)+fb*(sa(ie-4)+sa(ie)-res)+fc*(sa(ie-5)+recv(jk,2,1)-res)
+    sb(ie-2)=fa*(sa(ie-3)+sa(ie-1)-rof(2))+fb*(sa(ie-4)+sa(ie)-rof(2))+fc*(sa(ie-5)+recv(jk,2,1)-rof(2))
  end select
     sa(is)=sb(is)
     sa(is+1)=sb(is+1)-yl(is+1,2)*sa(is)
@@ -190,7 +189,7 @@
     sb(i)=yu(i,1)*sa(i)-yu(i,2)*sb(i+1)-yu(i,3)*sb(i+2)
  end do
  do i=is,ie
-    l=li(i); qa(l,m)=qa(l,m)+sb(i)
+    l=li(i); rr(l,1)=rr(l,1)+sb(i)
  end do
  end do
  end do
