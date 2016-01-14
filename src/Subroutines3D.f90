93ea96cfbd12008995c24b66bf188fc463c86c87
R. Perez Torro <rpt1g12@soton.ac.uk>
2016-01-14 15:00:26 +0000

    Formating

--- src/Subroutines3D.f90
+++ src/Subroutines3D.f90
@@ -22,46 +22,59 @@
     ir=0
  do nn=1,3; nz=(1-nrt)*(nn-1)+1
  select case(nn)
- case(1); send=>send1; recv=>recv1; case(2); send=>send2; recv=>recv2; case(3); send=>send3; recv=>recv3
+ case(1); send=>send1; recv=>recv1; 
+ case(2); send=>send2; recv=>recv2; 
+ case(3); send=>send3; recv=>recv3
  end select
  do ip=0,1; iq=1-ip; is=ip*ijk(1,nn); ie=1-2*ip
- select case(nbc(ip,nn)); case(35); ra0=zero; ii=1; case(40); ra0=zero; ii=0; case(45); ra0=n45; ii=1; end select
+ select case(nbc(ip,nn)); 
+ case(35); ra0=zero; ii=1; 
+ case(40); ra0=zero; ii=0;
+ case(45); ra0=n45; ii=1;
+ end select
  if(ndf(ip,nt,nn)==1) then
- do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
- do j=0,ijk(2,nn); jk=kp+j; l=indx3(is,j,k,nn)
-    res=ra0*rr(l,nz)
- do i=0,mp; l=indx3(is+ie*(i+ii),j,k,nn)
-    sap(i)=rr(l,nz)
- end do
-    send(jk,0,ip)=sum(pbco(0:mp,0,nt)*sap(0:mp))-res*pbcot(0,nt)
-    send(jk,1,ip)=sum(pbco(0:mp,1,nt)*sap(0:mp))-res*pbcot(1,nt)
-    send(jk,2,ip)=sap(0)-res
- end do
- end do
-    ir=ir+1; call MPI_ISEND(send(:,:,ip),3*nbsize(nn),MPI_REAL8,ncd(ip,nn),itag+iq,icom,ireq(ir),ierr)
-    ir=ir+1; call MPI_IRECV(recv(:,:,ip),3*nbsize(nn),MPI_REAL8,ncd(ip,nn),itag+ip,icom,ireq(ir),ierr)
+    do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
+       do j=0,ijk(2,nn); jk=kp+j; l=indx3(is,j,k,nn)
+          res=ra0*rr(l,nz)
+       do i=0,mp; l=indx3(is+ie*(i+ii),j,k,nn)
+          sap(i)=rr(l,nz)
+       end do
+          send(jk,0,ip)=sum(pbco(0:mp,0,nt)*sap(0:mp))-res*pbcot(0,nt)
+          send(jk,1,ip)=sum(pbco(0:mp,1,nt)*sap(0:mp))-res*pbcot(1,nt)
+          send(jk,2,ip)=sap(0)-res
+       end do
+    end do
+    ir=ir+1; call MPI_ISEND(send(:,:,ip),3*nbsize(nn),MPI_REAL8,&
+                            ncd(ip,nn),itag+iq,icom,ireq(ir),ierr)
+    ir=ir+1; call MPI_IRECV(recv(:,:,ip),3*nbsize(nn),MPI_REAL8,&
+                            ncd(ip,nn),itag+ip,icom,ireq(ir),ierr)
  end if
  end do
  end do
+
  if(ir/=0) then
     call MPI_WAITALL(ir,ireq,ista,ierr)
  end if
 
  if(n45==n45go) then
- do nn=1,3; nz=(1-nrt)*(nn-1)+1
- select case(nn); case(1); recv=>recv1; case(2); recv=>recv2; case(3); recv=>recv3; end select
- do ip=0,1; is=ip*ijk(1,nn)
- if(nbc(ip,nn)==45) then
- do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
- do j=0,ijk(2,nn); jk=kp+j; l=indx3(is,j,k,nn)
-    recv(jk,0,ip)=recv(jk,0,ip)+rr(l,nz)*pbcot(0,nt)
-    recv(jk,1,ip)=recv(jk,1,ip)+rr(l,nz)*pbcot(1,nt)
-    recv(jk,2,ip)=recv(jk,2,ip)+rr(l,nz)
- end do
- end do
- end if
- end do
- end do
+    do nn=1,3; nz=(1-nrt)*(nn-1)+1
+       select case(nn);
+       case(1); recv=>recv1;
+       case(2); recv=>recv2;
+       case(3); recv=>recv3;
+       end select
+       do ip=0,1; is=ip*ijk(1,nn)
+          if(nbc(ip,nn)==45) then
+             do k=0,ijk(3,nn); kp=k*(ijk(2,nn)+1)
+             do j=0,ijk(2,nn); jk=kp+j; l=indx3(is,j,k,nn)
+                recv(jk,0,ip)=recv(jk,0,ip)+rr(l,nz)*pbcot(0,nt)
+                recv(jk,1,ip)=recv(jk,1,ip)+rr(l,nz)*pbcot(1,nt)
+                recv(jk,2,ip)=recv(jk,2,ip)+rr(l,nz)
+             end do
+             end do
+          end if
+       end do
+    end do
  end if
 
  end subroutine mpigo
@@ -133,7 +146,8 @@
 
  integer(kind=ni),intent(in) :: nn,nz
  integer(kind=ni),parameter :: mfbi=1
- real(kind=nr),parameter,dimension(3) :: fex=(/45,-9,1/)/(mfbi*30.0_nr),ffx=(/4,6,7/)/(8*log(two))
+ real(kind=nr),parameter,dimension(3) :: fex=(/45,-9,1/)/(mfbi*30.0_nr),&
+                                         ffx=(/4,6,7/)/(8*log(two))
 
     nt=1; ns=ndf(0,1,nn); ne=ndf(1,1,nn)
 
